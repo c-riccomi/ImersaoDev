84aef2c7ce59d448f34c16d0fed0f193a99dce2d
Carolina Riccomi <84917787+c-riccomi@users.noreply.github.com>
2021-09-19 08:20:00 -0300

    Create script.js

---
+++ aluraflix1/script.js
@@ -0,0 +1,18 @@
+var listaFilmes = [
+  "https://i.ytimg.com/vi/U386EMeWo3I/movieposter.jpg",
+  "https://picfiles.alphacoders.com/137/137501.jpg",
+  "https://images2.vudu.com/poster2/195891-338",
+  "https://i.pinimg.com/originals/9a/05/ea/9a05eaff50a797faf53b0f8f046003d4.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/81O--m4AN1L._AC_SY879_.jpg",
+  "https://i.pinimg.com/originals/76/76/db/7676dbdb1e301137286cd1dcaba457bc.jpg",
+  "https://m.media-amazon.com/images/M/MV5BMTk5MDI5NTE4N15BMl5BanBnXkFtZTYwNjQ0NTE3._V1_FMjpg_UX1000_.jpg",
+  "https://lumiere-a.akamaihd.net/v1/images/image_a119dd78.jpeg",
+  "https://i.pinimg.com/originals/a3/10/a4/a310a49fde199ba03625275282dcac4b.jpg",
+  "https://m.media-amazon.com/images/I/61jFTTf9RBL._AC_SY500_.jpg",
+  "https://i.pinimg.com/originals/93/55/7f/93557f9394b184f697d4239ca9b4c1f6.png",
+  "https://resizing.flixster.com/13rq44o7ICTE-qFvZIpkIeHBdMU=/206x305/v2/https://flxt.tmsimg.com/assets/p8118537_p_v13_ao.jpg"
+];
+
+for (var i = 0; i < listaFilmes.length; i++) {
+  document.write("<img src=" + listaFilmes[i] + ">");
+}
